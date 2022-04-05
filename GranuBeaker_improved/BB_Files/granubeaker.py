0213a628e9d3ef6987118c24513bd317f6b332f2
Ben Jenkins <bdj746@student.bham.ac.uk>
2022-04-05 13:02:30 +0100

    Add while loop instead of for loop for multiple deletion steps

--- GranuBeaker_improved/BB_Files/granubeaker.py
+++ GranuBeaker_improved/BB_Files/granubeaker.py
@@ -29,24 +29,52 @@ sim = coexist.LiggghtsSimulation(script_path, verbose=True)
 # Run simulation up to given time (s)
 line = "\n" + "-" * 80 + "\n"
 
-print(line + "Pouring particles and letting them settle" + line)
+# Inserting Particles
+print(line + "Pouring particles" + line)
 sim.step_time(2.0)
 
-sim.execute_command("delete_atoms region 1")
+# Allowing particles to settle
+print(line + "Letting remaining particles fall and settle" + line)
+sim.step_time(1.0)
 
+# First deletion step
+radii_before_deletion = sim.radii()
+
+print(line + f"Deleting particles outside 50 ml region. Round: {1}" + line)
+sim.execute_command("delete_atoms region 1")
+radii_after_deletion = sim.radii()
+number_par_deleted = len(radii_before_deletion) - len(radii_after_deletion)
+
+# Delete particles until none are deleted anymore
+i = 1
+
+while number_par_deleted > 0:
+
+    radii_before_deletion = sim.radii()
+
+    print(line + f"Deleting particles outside 50 ml region. Round: {i+1}" + line)
+    sim.execute_command("delete_atoms region 1")
+    print(line + "Letting remaining particles settle" + line)
+    sim.step_time(1.0)
+
+    radii_after_deletion = sim.radii()
+    number_par_deleted = len(radii_before_deletion) - len(radii_after_deletion)
+
+    radii_before_deletion = []
+    radii_after_deletion = []
+
+    i = 1 + i
 
 # Extract particle properties as NumPy arrays
 time = sim.time()
 radii = sim.radii()
 positions = sim.positions()
-velocities = sim.velocities()
 
 print("\n\n" + line)
-print(f"Simulation time: {time} s\nParticle positions:\n{positions}")
+print(f"Simulation time: {time} s\nNumber of Particles:{len(positions)} \nNumber of Deletions: {i}")
 print(line + "\n\n")
 
 
 # Save results as efficient binary NPY-formatted files
-np.save(f"results/radii.npy", radii)
-np.save(f"results/positions.npy", positions)
-np.save(f"results/velocities.npy", velocities)
+np.save(f"{results_dir}/radii.npy", radii)
+np.save(f"{results_dir}/positions.npy", positions)
