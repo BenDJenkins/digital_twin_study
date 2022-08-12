5fd521cf564d080d100a820a402f0fe454e5d20c
Ben Jenkins <bdj746@student.bham.ac.uk>
2022-08-12 09:11:00 +0200

    Update plot_gd_data.py to use gd data

--- GranuDrum/Analysis/plot_gd_data.py
+++ GranuDrum/Analysis/plot_gd_data.py
@@ -6,7 +6,7 @@ from plotly.subplots import make_subplots
 import plotly
 
 # User dependent variables
-filepath = 'gb_particledata'
+filepath = 'data/gd_data_large_30rpm'
 rolling_titles = ['Rolling friction: 0.0', 'Rolling friction: 0.001', 'Rolling friction: 0.01',
                   'Rolling friction: 0.1', 'Rolling friction: 0.2', 'Rolling friction: 0.4', 'Rolling friction: 0.7']
 slidingfrictionvals = [0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 0.8]
@@ -31,12 +31,12 @@ for r in range(7):
     for s in range(7):
         focus = (filtered_data.rollingfriction == r) & (filtered_data.slidingfriction == s)
         cohesion = [0, 5000, 10000, 20000, 40000, 70000, 100000]
-        particlenumber = filtered_data.no_particles[focus]
+        cohesiveindex = filtered_data.cohesiveindex[focus]
 
         if r == 0:
             fig.add_trace(go.Scatter(name=f'Sliding friction: {slidingfrictionvals[s]}',
                                      x=cohesion,
-                                     y=particlenumber,
+                                     y=cohesiveindex,
                                      line=dict(width=4, dash='dash', color=cols[s]),
                                      marker=dict(color=cols[s]),
                                      legendgroup='group1'
@@ -45,7 +45,7 @@ for r in range(7):
                           col=r+1)
         else:
             fig.add_trace(go.Scatter(x=cohesion,
-                                     y=particlenumber,
+                                     y=cohesiveindex,
                                      line=dict(width=4, dash='dash', color=cols[s]),
                                      marker=dict(color=cols[s]),
                                      legendgroup='group1',
@@ -54,18 +54,18 @@ for r in range(7):
                           row=1,
                           col=r+1)
 
-med_coh = np.arange(0, 100000, 10000)
-
-for r in range(7):
-    for s in range(7):
-        restitution = 0.9
-        slidingfriction = slidingfrictionvals[s]
-        rollingfriction = rollingfrictionvals[r]
-        par = ((14364.258 + (-102.00902 * slidingfriction)) + ((-17.655941 * slidingfriction) * rollingfriction))
-        par_num = [par]*len(med_coh)
-        fig.add_trace(go.Scatter(x=med_coh, y=par_num,
-                      line=dict(color='black')),
-                      row=1,
-                      col=r+1)
+# med_coh = np.arange(0, 100000, 10000)
+#
+# for r in range(7):
+#     for s in range(7):
+#         restitution = 0.9
+#         slidingfriction = slidingfrictionvals[s]
+#         rollingfriction = rollingfrictionvals[r]
+#         par = ((14364.258 + (-102.00902 * slidingfriction)) + ((-17.655941 * slidingfriction) * rollingfriction))
+#         par_num = [par]*len(med_coh)
+#         fig.add_trace(go.Scatter(x=med_coh, y=par_num,
+#                       line=dict(color='black')),
+#                       row=1,
+#                       col=r+1)
 
 fig.show()
